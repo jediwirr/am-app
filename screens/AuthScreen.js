2b98c5bdb95af62923d2719bfcbf502a52ce0ace
anteon <andm1793@gmail.com>
2021-09-27 15:40:42 +0300

    loads

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -7,7 +7,7 @@ import {
     Image,
     Alert,
     KeyboardAvoidingView,
-    Platform
+    Dimensions
 } from 'react-native';
 import { Button} from 'react-native-paper';
 
@@ -20,6 +20,7 @@ const AuthScreen = () => {
     const log_in = (students, user, user_type, user_data) => dispatch({type: 'LOG_IN', students, user, user_type, user_data});
     const [login, onChangeLogin] = useState('');
     const [password, onChangePassword] = useState('');
+    const {height} = Dimensions.get('screen');
 
     const sendCredentials = () => {
         fetch(`https://diary.alma-mater-spb.ru/e-journal/api/check_login.php?username=${login}&password=${password}&token=alma831`, {
@@ -43,47 +44,43 @@ const AuthScreen = () => {
     };
 
     return (
-        <View
-            style={{ ...styles.container, backgroundColor: '#00656d' }}
-        >
-
-            <View style={{ flexDirection: 'row', marginBottom: 15 }}>
-                <Text style={styles.logoText}>Гимназия «Альма Матер»</Text>
-                <Image
-                    style={styles.logo}
-                    source={require('../assets/gimnazist.png')}
-                />
-            </View>
-            <Text style={styles.greeting}>Добро пожаловать!</Text>
-            <View>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={login => onChangeLogin(login)}
-                    value={login}
-                    placeholder='Введите логин'
-                    autoCapitalize='none'
-                />
-                <TextInput
-                    style={styles.input}
-                    onChangeText={password => onChangePassword(password)}
-                    value={password}
-                    placeholder='Введите пароль'
-                    secureTextEntry={true}
-                />
-            </View>
-            <Button
-                onPress={sendCredentials}
-                color='#fff'
-                uppercase={false}
-                style={{ padding: 10 }}
-                labelStyle={{ fontSize: 25 }}
-            >
-                Вход
-            </Button>
+        <KeyboardAvoidingView style={{...styles.container, backgroundColor: '#00656d'}}>
+                <View style={{ flexDirection: 'row', marginBottom: 15 }}>
+                    <Text style={styles.logoText}>Гимназия «Альма Матер»</Text>
+                    <Image
+                        style={styles.logo}
+                        source={require('../assets/gimnazist.png')}
+                    />
+                </View>
+                <Text style={styles.greeting}>Добро пожаловать!</Text>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={login => onChangeLogin(login)}
+                        value={login}
+                        placeholder='Введите логин'
+                        autoCapitalize='none'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={password => onChangePassword(password)}
+                        value={password}
+                        placeholder='Введите пароль'
+                        secureTextEntry={true}
+                    />
+                </View>
+                <Button
+                    onPress={sendCredentials}
+                    color='#fff'
+                    uppercase={false}
+                    style={{ padding: 10 }}
+                    labelStyle={{ fontSize: 25 }}
+                >
+                    Вход
+                </Button>
 
             <Links col='#fff' />
-
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
